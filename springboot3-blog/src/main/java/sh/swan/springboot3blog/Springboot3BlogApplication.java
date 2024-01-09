8e5d2d99bcfad0803ba0a952f4bd68363acd831a
ksh4030 <kei7424030@gmail.com>
2024-01-09 23:33:02 +0900

    init project

---
+++ springboot3-blog/src/main/java/sh/swan/springboot3blog/Springboot3BlogApplication.java
@@ -0,0 +1,13 @@
+package sh.swan.springboot3blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3BlogApplication.class, args);
+    }
+
+}
---
+++ springboot3-blog/src/test/java/sh/swan/springboot3blog/Springboot3BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.swan.springboot3blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
